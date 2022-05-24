91ddc96da52a4fef8bfb14f39b040d7e5b98e0cd
altayozyavas <altayozyavas@gmail.com>
2022-05-23 20:34:19 -0500

    minor fixes

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const body = document.getElementsByTagName("body")[0];
-body.addEventListener("click", () => {
+const calc = document.getElementById("calc");
+calc.addEventListener("click", () => {
     const principal = document.getElementById("amount").value;
     const interest = document.getElementById("rate").value;
     const months = document.getElementById("months").value;
+    if (principal == "" || interest == "" || months == "") {
+        alert("Please enter valid input!");
+    }
     const monthlyPayment = ((principal) * ((interest / (100 * (12 / months))) + 1)) / months;
     if (!isNaN(monthlyPayment) && monthlyPayment < Infinity) {
         document.getElementById("total").innerHTML = `Monthly Payment: $${monthlyPayment.toFixed(2)}`;
+    } else {
+        document.getElementById("total").innerHTML = "Invalid input"
     }
 })
